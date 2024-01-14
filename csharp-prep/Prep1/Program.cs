03db0abac6dd4011375d9e1190d4b1b3b9e46214
zachmills135 <zachmills135@gmail.com>
2024-01-13 23:13:07 -0700

    Finished the project for the first prep assignment.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first given name? ");
+        string givenName = Console.ReadLine();
+
+        Console.WriteLine("What is your surname / last name? ");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {givenName} {surname}.");
     }
 }
