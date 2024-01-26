97673a8bd9fda78df643b7131c0ce0b6a957a3f9
zachmills135 <zachmills135@gmail.com>
2024-01-25 22:21:31 -0700

    Added more comments to the prepare activity.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -6,24 +6,25 @@ public class Fraction{
     private int _top;
     private int _bottom;
 
-    // Below is the constructor:
+    // Below is a constructor with no parameters:
     public Fraction(){
         _top = 1;
         _bottom = 1;
     }
-
+    
+    // Below is a constructor with one parameter:
     public Fraction(int wholeNumber){
         _top = wholeNumber;
         _bottom = 1;
     }
 
+    // Below is a constructor with both parameters:
     public Fraction(int top,int bottom){
         if (bottom == 0)
             throw new ArgumentException("The denominator cannot be zero.");
 
             _top = top;
             _bottom = bottom;
-        
     }
 
     public int GetTop(){
